7acea1881b25b6cb73553308ee2b89e0418d6f85
vedhavyas <vedhavyas.singareddi@gmail.com>
2018-01-09 16:12:45 +0530

    add encrypt and decrypt

--- security.go
+++ security.go
@@ -1,7 +1,13 @@
 package twothy
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
 
 	"golang.org/x/crypto/ssh/terminal"
 )
@@ -22,11 +28,9 @@ func getPassword() ([]byte, error) {
 
 // addPadding repeat adds the passwordPad to pwd
 func addPadding(pwd []byte, repeat int) []byte {
-	for i := 0; i < repeat; i++ {
-		pwd = append(pwd, passwordPad)
-	}
-
-	return pwd
+	pad := []byte{passwordPad}
+	pad = bytes.Repeat(pad, repeat)
+	return append(pwd, pad...)
 }
 
 // validatePassword ensures password is either 16, 24, 32 bytes long
@@ -42,4 +46,44 @@ func validatePassword(pwd []byte) ([]byte, error) {
 	default:
 		return nil, fmt.Errorf("password should be <=32 characters: %d", len(pwd))
 	}
+}
+
+// encrypt encrypts the message with AES
+func encrypt(msg, key []byte) (string, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		// this should not happen
+		return "", err
+	}
+
+	ct := make([]byte, aes.BlockSize+len(msg))
+	iv := ct[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	cfb := cipher.NewCFBEncrypter(block, iv)
+	cfb.XORKeyStream(ct[aes.BlockSize:], msg)
+	return base64.URLEncoding.EncodeToString(ct), nil
+}
+
+// decrypt decrypts the cipher text to plaintext
+func decrypt(msg string, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	decodedMsg, err := base64.URLEncoding.DecodeString(msg)
+	if err != nil {
+		return nil, err
+	}
+
+	iv := decodedMsg[:aes.BlockSize]
+	data := decodedMsg[aes.BlockSize:]
+
+	cfb := cipher.NewCFBDecrypter(block, iv)
+	cfb.XORKeyStream(data, data)
+
+	return data, nil
 }
--- security_test.go
+++ security_test.go
@@ -96,3 +96,31 @@ func Test_validatePassword(t *testing.T) {
 		}
 	}
 }
+
+func Test_encrypt_decrypt(t *testing.T) {
+	tests := []string{
+		"hello, world!!",
+		"This is a long messages",
+		"",
+		"take this!!!!",
+		"this is a even longer message. Longer longer longer!!!!",
+	}
+
+	password := addPadding([]byte("password"), 8)
+
+	for _, c := range tests {
+		ed, err := encrypt([]byte(c), password)
+		if err != nil {
+			t.Fatalf("unexpected error while encrypting: %v", err)
+		}
+
+		dd, err := decrypt(ed, password)
+		if err != nil {
+			t.Fatalf("unexpected error while decrypting: %v", err)
+		}
+
+		if string(dd) != c {
+			t.Fatalf("expected '%s' but got '%s'", c, string(dd))
+		}
+	}
+}
